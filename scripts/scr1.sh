7e3f84e9b2546bc16a4e29d73453a0f511225349
Tom Hlava <hlava@cesnet.cz>
2020-08-27 22:19:51 +0200

    added d1.txt and d2.txt + trig6

--- scripts/scr1.sh
+++ scripts/scr1.sh
@@ -4,8 +4,8 @@ echo "scr1.sh"
 
 DATE=$(date '+%y%m%d-%H%M%S')
 echo ">$DATE<"
-echo "$DATE" > ./tmp/date.txt
+echo "$DATE" | tee ./tmp/date.txt | tee ./d1.txt > ./d2.txt
 ls -l ./tmp/date.txt
-cat ./tmp/date.txt
+cat ./tmp/date.txt ./d1.txt ./d2.txt
 
 echo "Done."
