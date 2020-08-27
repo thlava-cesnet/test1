3a8126f2c586e5ea1b21319975a7b331677a6f92
Tom Hlava <hlava@cesnet.cz>
2020-08-27 22:24:10 +0200

    d1/d2 modev to test2 + trig8

--- scripts/scr1.sh
+++ scripts/scr1.sh
@@ -4,8 +4,8 @@ echo "scr1.sh"
 
 DATE=$(date '+%y%m%d-%H%M%S')
 echo ">$DATE<"
-echo "$DATE" | tee ./tmp/date.txt | tee ./d1.txt > ./d2.txt
+echo "$DATE" | tee ./tmp/date.txt | tee ./test2/d1.txt > ./test2/d2.txt
 ls -l ./tmp/date.txt
-cat ./tmp/date.txt ./d1.txt ./d2.txt
+cat ./tmp/date.txt ./test2/d1.txt ./test2/d2.txt
 
 echo "Done."
