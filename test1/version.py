bb2b1496505fddd573426534f07fe8edca65834b
Tom Hlava <email = hlava@cesnet.cz>
2021-07-29 12:18:05 +0200

    wf4 added

---
+++ test1/version.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 CESNET.
+#
+# OARepo-Tokens is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Version information for OARepo-Tokens.
+
+This file is imported by ``oarepo_tokens.__init__``,
+and parsed by ``setup.py``.
+"""
+
+__version__ = '0.1.0'
