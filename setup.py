2e7c4a5073c4885e60bc8a8f87d3e29db3f29851
Tom Hlava <hlava@cesnet.cz>
2022-02-10 18:07:50 +0100

    test

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+#        'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
