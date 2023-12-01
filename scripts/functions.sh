db684726c437bae9f61bcdd963717268dfb5fb77
Tom Hlava <hlava@cesnet.cz>
2023-12-01 16:08:01 +0100

    test notif.

---
+++ scripts/functions.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function api_call(){
+  local url="$1"
+  shift
+  local method="${1:-GET}"
+  shift
+  local data="{${BR:+ \"ref\":\"$BR\"}}"
+  curl -s -X$method -H "Accept: application/vnd.github.v3+json" ${TOK:+ -H "authorization: Bearer $TOK"} -d "$data" "$url" $@
+}
+
+function mm_msg(){
+  local msg="${1:-Incoming Webhook test $(date '+%y%m%d-%H%M%S')}"
+  shift
+  local channel="$1"
+  shift
+  local url="${MM_OAREPO_WH:?Error, MM_OAREPO_WH undefined}"
+  local opts='-H "Content-Type: application/json"'
+  local data="{\"text\":\"$msg\"${channel:+, \"channel\":\"$channel\"}}"
+  curl -i -X POST -H "Content-Type: application/json" -d "$data" "$url"
+}
